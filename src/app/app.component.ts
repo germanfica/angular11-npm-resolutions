64c0986f07044e8832dfdc58c431aa87fd21307f
German Fica <germafica@gmail.com>
2024-07-16 01:02:29 -0300

    Add experimentalexperimental  packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { BlockBlobClient} from '@azure/storage-blob';
+import { NgSelectComponent } from '@ng-select/ng-select';
+import { TranslateService } from '@ngx-translate/core';
+import { ICellEditorAngularComp } from 'ag-grid-angular';
+import { IAfterGuiAttachedParams, ICellEditorParams } from 'ag-grid-community';
+import { ColDef, ColGroupDef } from 'ag-grid-community';
+
+
 
 // const BlockBlobClient = require("@azure/storage-blob");
 
@@ -15,6 +22,36 @@ import { BlockBlobClient} from '@azure/storage-blob';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements ICellEditorAngularComp {
+  getValue() {
+    throw new Error('Method not implemented.');
+  }
+  isPopup?(): boolean {
+    throw new Error('Method not implemented.');
+  }
+  getPopupPosition?(): string | undefined {
+    throw new Error('Method not implemented.');
+  }
+  isCancelBeforeStart?(): boolean {
+    throw new Error('Method not implemented.');
+  }
+  isCancelAfterEnd?(): boolean {
+    throw new Error('Method not implemented.');
+  }
+  focusIn?(): void {
+    throw new Error('Method not implemented.');
+  }
+  focusOut?(): void {
+    throw new Error('Method not implemented.');
+  }
+  getFrameworkComponentInstance?() {
+    throw new Error('Method not implemented.');
+  }
+  agInit(params: ICellEditorParams): void {
+    throw new Error('Method not implemented.');
+  }
+  afterGuiAttached?(params?: IAfterGuiAttachedParams): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'angular11-node16';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { AgGridModule } from 'ag-grid-angular';
+import { BackButtonDisableModule } from 'angular-disable-browser-back-button';
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgSelectModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+
+    SweetAlert2Module.forRoot(),
+
+    BackButtonDisableModule.forRoot({
+      preserveScrollPosition: true
+    }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
